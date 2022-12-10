19e18040113c6d49dc3f153f75bff4312322bfce
Renato Alves Soares <renatoguara2020@gmail.com>
2022-12-10 10:35:27 -0300

    ok

--- connection.php
+++ connection.php
@@ -4,9 +4,20 @@
   define('DB_PASSWORD', '');
   define('DB_NAME', 'student');
 
-  $conn = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
+  $conn =new mysqli(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
 
   if ($conn === false) {
-      die("ERROR: Could not connect. " . mysqli_connect_error());
+      die("ERROR: Could not connect. " .mysqli_connect_error());
   }
+  $conn->set_charset("utf8mb4");
+
+// mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
+// try {
+//   $mysqli = new mysqli(DB_SERVER,  DB_USERNAME, DB_PASSWORD,DB_NAME);
+//   $mysqli->set_charset("utf8mb4");
+// } catch(Exception $e) {
+//   error_log($e->getMessage());
+//   exit('Error connecting to database'); //Should be a message a typical user could understand
+// }
+
 ?>
---
+++ crudPDO.php
@@ -0,0 +1,26 @@
+<?php
+$servername = "localhost";
+$username = "username";
+$password = "password";
+$dbname = "myDBPDO";
+
+try {
+  $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+  // set the PDO error mode to exception
+  $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  $sql = "INSERT INTO MyGuests (firstname, lastname, email)VALUES (':firstname', ':lastname', ':email')";
+  $stmt->bindParam(':firstname', $firstname);
+  $stmt->bindParam(':lastname', $lastname);
+  $stmt->bindParam(':email', $email);
+  // use exec() because no results are returned
+  $conn->exec($sql);
+  echo "New record created successfully";
+  $result = $stmt->setFetchMode(PDO::FETCH_ASSOC);
+  
+  
+} catch(PDOException $e) {
+  echo $sql . "<br>" . $e->getMessage();
+}
+
+$conn = null;
+?>
